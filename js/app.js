417fd578290423221de4a61089b1d76a45015c73
Artem Khannanov <Artem.Khannanov@coral-club>
2023-11-02 15:34:42 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,82 @@
+// // Тултип
+// function copyClipboard(text) {
+//   return navigator.clipboard.writeText(text.toUpperCase())
+// }
+
+// const footerTooltip = document.querySelector('.footer__tooltip')
+// const footerCopyLink = document.querySelector('.footer__top-link')
+
+// footerCopyLink.addEventListener('mouseenter', () => {
+//   footerTooltip.classList.add('tooltip_visible')
+// })
+
+
+// footerCopyLink.addEventListener('mouseup', () => {
+//   footerCopyLink.style.background = ''
+// })
+
+// footerCopyLink.addEventListener('mouseleave', () => {
+//   footerTooltip.classList.remove('tooltip_visible')
+//   footerTooltip.textContent = 'Копировать'
+// })
+
+// footerCopyLink.addEventListener('click', function() {
+//   copyClipboard('coralapp')
+//   footerTooltip.classList.add('tooltip_visible')
+//   footerTooltip.textContent = 'Скопировано'
+// })
+
+
+// // Ссылка на скачивание (хедер, футер)
+// const appleLink = document.querySelectorAll('.appropriate__apple')
+// const androidLink = document.querySelectorAll('.appropriate__android')
+// const othersLink = document.querySelectorAll('.appropriate__others')
+
+// const visibleShopImgHeader = document.querySelector('.header__bottom-store--link-showed img')
+// const visibleShopLinkHeader = document.querySelector('.header__bottom-store--link-showed')
+
+// const visibleShopImgFooter = document.querySelector('.footer__bottom_link--showed img')
+// const visibleShopLinkFooter = document.querySelector('.footer__bottom_link--showed')
+
+// if (window.screen.width < 1024) {
+//   if (/iPhone/i.test(navigator.userAgent)) {
+//     visibleShopImgHeader.src = 'img/download_appstore.svg';
+//     visibleShopLinkHeader.href = 'https://apps.apple.com/ru/app/coral-club/id1437262333'
+
+//     visibleShopImgFooter.src = 'img/download_appstore.svg';
+//     visibleShopLinkFooter.href = 'https://apps.apple.com/ru/app/coral-club/id1437262333'
+//   } else if (/Android/i.test(navigator.userAgent)) {
+//     visibleShopImgHeader.src = 'img/download_playmarket.svg';
+//     visibleShopLinkHeader.href = 'https://play.google.com/store/apps/details?id=com.coralclub.distribution.app&hl=ru'
+
+//     visibleShopImgFooter.src = 'img/download_playmarket.svg';
+//     visibleShopLinkFooter.href = 'https://play.google.com/store/apps/details?id=com.coralclub.distribution.app&hl=ru'
+//   } else {
+//     visibleShopImgHeader.src = 'img/download_rustore.svg';
+//     visibleShopLinkHeader.href = 'https://apps.rustore.ru/app/com.coralclub.distribution.app'
+
+//     visibleShopImgFooter.src = 'img/download_rustore.svg';
+//     visibleShopLinkFooter.href = 'https://apps.rustore.ru/app/com.coralclub.distribution.app'
+//   }
+// }
+
+
+// // const burgerClose = document.querySelector('.burger__btn')
+// // const menuBurger = document.querySelector('.header__menu-burger')
+// // const menuClose = document.querySelector('.header__menu-top--close')
+// // const header = document.querySelector('header')
+
+// // burgerClose.addEventListener('click', function() {
+// //   menuBurger.classList.add('header__menu-burger-active')
+// // })
+
+// // menuClose.addEventListener('click', function() {
+// //     menuBurger.classList.remove('header__menu-burger-active')
+// // })
+
+// // document.addEventListener('click', function(e) {
+// //   let target = e.target;
+// //   if (!target.closest(".container")) {
+// //       menuBurger.classList.remove('header__menu-burger-active');
+// //   }
+// // })
---
+++ js/swiper.js
@@ -0,0 +1,90 @@
+let swiperGor = new Swiper('.swiper__gor', {
+    // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // direction: 'vertical',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+// let heading = document.querySelector('.header__bottom-title')
+// let HEADER = document.getElementById('header')
+// let headerLogo = document.querySelector('.header__top-logo--img')
+// let headerMenuLinks = document.querySelectorAll('.header__link')
+// let headerBackBtn = document.querySelector('.header__top-back--btn')
+// let headerBackSpan = document.querySelector('.header__top-back--btn span')
+// let headerBottomTitleLight = document.querySelector('.header__bottom-title--light')
+// let headerBottomTextLight = document.querySelector('.header__bottom-text--light')
+
+// let animationAppear = document.querySelector('.animation__appear')
+
+// let burgerBtn = document.querySelector('.burger__btn')
+
+// const SVH = document.querySelector('.header__top-back img')
+
+// const directSection = document.querySelector('.direct')
+// const sliderVertical = document.getElementById('slider-vertical')
+
+
+// // amimation
+// const animation_1 = document.querySelector('.swiper-slide_amimated_1')
+// const animation_2 = document.querySelector('.swiper-slide_amimated_2')
+// const animation_3 = document.querySelector('.swiper-slide_amimated_3')
+
+
+// let swiperG = new Swiper('.swiper__app_g', {
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true
+//   },
+
+//   // direction: 'vertical',
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },   
+// });
+
+
+// // amimation
+// const animation_4 = document.querySelector('.swiper-slide_amimated_4')
+// const animation_5 = document.querySelector('.swiper-slide_amimated_5')
+// const animation_6 = document.querySelector('.swiper-slide_amimated_6')
+
+// swiperG.on('activeIndexChange', function(anim) {
+//   if (swiperG.activeIndex === 0) {
+//     animation_4.classList.add('animate__bounceInRight')
+//   } else {
+//     animation_4.classList.remove('animate__bounceInRight')
+//   } 
+//   if (swiperG.activeIndex === 1) {
+//     animation_5.classList.add('animate__bounceInRight')
+//   } else {
+//     animation_5.classList.remove('animate__bounceInRight')
+//   }
+//   if (swiperG.activeIndex === 2) {
+//     animation_6.classList.add('animate__bounceInRight')
+//   } else {
+//     animation_6.classList.remove('animate__bounceInRight')
+//   }
+// })
